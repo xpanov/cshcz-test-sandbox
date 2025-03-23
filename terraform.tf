ddc3ad1c762e99f24ae4e9723faca0fa5610af7e
Alex Pascal <futupas@gmail.com>
2025-03-23 14:55:26 +0100

    upd: some fixes, added variables

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,60 @@
 terraform {
+  backend "http" {
+  }
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
+    kypo = {
+      source  = "vydrazde/kypo"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+    }
+    gitlab = {
+      source  = "gitlabhq/gitlab"
     }
   }
 }
 
-provider "local" {}
+provider "kypo" {
+  endpoint = var.KYPO_ENDPOINT
+}
 
-resource "local_file" "test_file" {
-  filename = "test.txt"
-  content  = "Hello, Terraform!"
+provider "openstack" {
+  auth_url = "https://identity.cloud.muni.cz/v3"
+  region   = "brno1"
+}
+
+provider "gitlab" {
+  base_url = "https://${var.CI_SERVER_HOST}/api/v4"
+  token    = var.ACCESS_TOKEN
+}
+
+variable "KYPO_ENDPOINT" {}
+variable "CI_PROJECT_ID" {}
+variable "CI_PROJECT_URL" {}
+variable "CI_COMMIT_SHORT_SHA" {}
+variable "CI_SERVER_HOST" {}
+variable "CI_PROJECT_PATH" {}
+variable "NAME" {}
+variable "TYPE" {}
+variable "DISTRO" {}
+variable "GUI_ACCESS" {}
+variable "ACCESS_TOKEN" {
+  sensitive = true
+}
+
+module "topology" {
+  source               = "gitlab.ics.muni.cz/muni-kypo-images/image-testing-topology/kypo"
+  kypo_endpoint        = var.KYPO_ENDPOINT
+  project_url          = "${var.CI_SERVER_HOST}/${var.CI_PROJECT_PATH}"
+  project_access_token = var.ACCESS_TOKEN
+  project_id           = var.CI_PROJECT_ID
+  rev                  = var.CI_COMMIT_SHORT_SHA
+  image_name           = var.NAME
+  os_type              = var.TYPE
+  os_distro            = var.DISTRO
+  gui_access           = var.GUI_ACCESS
+}
+
+output "pool_url" {
+  value = module.topology.pool_url
 }
